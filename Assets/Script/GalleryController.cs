4d1e985b2cf1679b67999351595de81da0cddd19
Chaoyifu <chaoyifu@asu.edu>
2017-03-26 00:13:31 -0700

    Unknown

---
+++ Assets/Script/GalleryController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GalleryController : MonoBehaviour {
+
+    public ArrayList gallery = new ArrayList<GameObject>();
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    void OnMouseDrag()
+    {
+
+    }
+}
