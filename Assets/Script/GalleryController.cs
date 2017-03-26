97e479a128e427bfb51f722df8e044cf0040c3e3
Chaoyifu <chaoyifu@asu.edu>
2017-03-26 02:28:17 -0700

    Final version

--- Assets/Script/GalleryController.cs
+++ Assets/Script/GalleryController.cs
@@ -4,14 +4,63 @@ using UnityEngine;
 
 public class GalleryController : MonoBehaviour {
 
-    public ArrayList gallery = new ArrayList<GameObject>();
+    public GameObject[] GalleryItem;
+    private float lastX;
 	// Use this for initialization
 	void Start () {
-		
-	}
+    }
 
+    void OnMouseDown() {
+        lastX = Input.mousePosition.x;
+        Debug.Log(lastX);
+
+        if (GameInfo.gameState == 1) {
+            Application.LoadLevel(Application.loadedLevel + 1);
+        }
+        if (GameInfo.gameState == 2)
+        {
+            Application.LoadLevel(Application.loadedLevel + 2);
+        }
+    }
     void OnMouseDrag()
     {
-
+        float distance = Input.mousePosition.x - lastX;
+        Debug.Log(distance);
+        if (distance < -0.5f) {
+            for (int i = 0; i < GalleryItem.Length; i++) {
+                if (GalleryItem[i].transform.rotation.y < 45f)
+                {
+                    GalleryItem[i].transform.Rotate(0, 45f, 0);
+                    if (GalleryItem[i].transform.rotation.y == 90f)
+                        GalleryItem[i].transform.position = new Vector3(GalleryItem[i].transform.position.x + 0.6f, GalleryItem[i].transform.position.y, GalleryItem[i].transform.position.z + 0.2f);
+                    else
+                        GalleryItem[i].transform.position = new Vector3(GalleryItem[i].transform.position.x + 0.6f, GalleryItem[i].transform.position.y, GalleryItem[i].transform.position.z);
+                }
+                else
+                {
+                    GalleryItem[i].transform.Rotate(0, -90f, 0);
+                    GalleryItem[i].transform.position = new Vector3(GalleryItem[i].transform.position.x - 1.2f, GalleryItem[i].transform.position.y, GalleryItem[i].transform.position.z);
+                }
+            }
+        }
+        if (distance > 0.5f)
+        {
+            for (int i = 0; i < GalleryItem.Length; i++)
+            {
+                if (GalleryItem[i].transform.rotation.y > -45f)
+                {
+                    GalleryItem[i].transform.Rotate(0, -45f, 0);
+                    if (GalleryItem[i].transform.rotation.y == 90f)
+                        GalleryItem[i].transform.position = new Vector3(GalleryItem[i].transform.position.x + 0.6f, GalleryItem[i].transform.position.y, GalleryItem[i].transform.position.z + 0.2f);
+                    else
+                        GalleryItem[i].transform.position = new Vector3(GalleryItem[i].transform.position.x + 0.6f, GalleryItem[i].transform.position.y, GalleryItem[i].transform.position.z);
+                }
+                else
+                {
+                    GalleryItem[i].transform.Rotate(0, 90f, 0);
+                    GalleryItem[i].transform.position = new Vector3(GalleryItem[i].transform.position.x + 1.2f, GalleryItem[i].transform.position.y, GalleryItem[i].transform.position.z);
+                }
+            }
+        }
     }
 }
---
+++ Assets/Script/toggleToGameScene2.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRStandardAssets.Utils;
+
+namespace VRStandardAssets.Examples
+{
+    // This script is a simple example of how an interactive item can
+    // be used to change things on gameobjects by handling events.
+    public class toggleToGameScene2 : MonoBehaviour
+    {
+        [SerializeField]
+        private VRInteractiveItem m_InteractiveItem;
+
+        private AudioSource Sound;
+        private void Awake()
+        {
+        }
+
+
+        private void OnEnable()
+        {
+            m_InteractiveItem.OnClick += HandleClick;
+        }
+
+
+        private void OnDisable()
+        {
+            m_InteractiveItem.OnClick -= HandleClick;
+        }
+
+
+        //Handle the Click event
+        private void HandleClick()
+        {
+            GameInfo.gameState = 2;
+            Application.LoadLevel(Application.loadedLevel + 1);
+        }
+    }
+
+}
